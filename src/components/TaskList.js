faba43999ec98628238a217c23ac4962227776ec
Yonas Fesehatsion <yonas.fesehatsion@ferguson.com>
2021-09-02 10:42:24 -0400

    container component added

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Task from './Task';
 
-export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+import { connect } from 'react-redux';
+import { archiveTask, pinTask } from '../lib/redux';
+
+export function PureTaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+  console.log("onArchiveTask", onArchiveTask);
+  console.log("tasks", tasks);
+  console.log("onPinTask", onPinTask);
   const events = {
     onPinTask,
     onArchiveTask,
@@ -51,3 +58,30 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
     </div>
   );
 }
+
+PureTaskList.propTypes = {
+    /** Checks if it's in loading state */
+    loading: PropTypes.bool,
+    /** The list of tasks */
+    tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+    /** Event to change the task to pinned */
+    onPinTask: PropTypes.func.isRequired,
+    /** Event to change the task to archived */
+    onArchiveTask: PropTypes.func.isRequired,
+  };
+  
+  PureTaskList.defaultProps = {
+    loading: false,
+  };
+  
+  export default connect(
+    ({ tasks }) => ({
+      tasks: tasks.filter(
+        (t) => t.state === "TASK_INBOX" || t.state === "TASK_PINNED"
+      ),
+    }),
+    (dispatch) => ({
+      onArchiveTask: (id) => dispatch(archiveTask(id)),
+      onPinTask: (id) => dispatch(pinTask(id)),
+    })
+  )(PureTaskList);
